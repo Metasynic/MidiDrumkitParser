b3d77a2b7c8726f90c3769ad30c19ee0f492b807
Metasynic <samwilliams1248@gmail.com>
2020-01-07 17:47:29 +0000

    Create HierarchicalRhythmInferrer class.

---
+++ HierarchicalRhythmInferrer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIDI_Drumkit_Parser
+{
+    public class HierarchicalRhythm
+    {
+
+    }
+
+    public static class HierarchicalRhythmInferrer
+    {
+
+    }
+}
